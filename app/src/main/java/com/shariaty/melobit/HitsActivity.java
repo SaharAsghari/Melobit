67bca983283fc72e865e44370845d2fe46f26e4d
sahar <Sahar.AsghariEn@gmail.com>
2023-01-20 01:08:06 +0330

    Add HitsActivity.java

--- app/src/main/java/com/shariaty/melobit/HitsActivity.java
+++ app/src/main/java/com/shariaty/melobit/HitsActivity.java
@@ -1,14 +1,74 @@
 package com.shariaty.melobit;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.shariaty.melobit.adapters.SongAdapter;
+import com.shariaty.melobit.data.SongResponse;
 
 public class HitsActivity extends AppCompatActivity {
+
+    private RecyclerView rvTodayHits, rvThisWeekHits;
+    private TextView tvError;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hits);
+        rvTodayHits = findViewById(R.id.rv_today);
+        rvThisWeekHits = findViewById(R.id.rv_week);
+        tvError = findViewById(R.id.tv_error);
+
+        LinearLayoutManager layoutManager
+                = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        rvTodayHits.setLayoutManager(layoutManager);
+        LinearLayoutManager layoutManager1
+                = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        rvThisWeekHits.setLayoutManager(layoutManager1);
+
+        RequestManager manager = new RequestManager(this);
+        SongListRequestListener todayListener = new SongListRequestListener() {
+            @Override
+            public void didFetch(SongResponse response) {
+                rvTodayHits.setAdapter(new SongAdapter(HitsActivity.this, response.getResults(),
+                        new SongAdapter.ClickListener() {
+                            @Override
+                            public void onSongClick(int position, View v, String id) {
+                                Intent intent = new Intent(HitsActivity.this, SongActivity.class);
+                                intent.putExtra("id", id);
+                                startActivity(intent);
+                            }
+                        }));
+            }
+
+            @Override
+            public void didError(String errorMessage) {
+                tvError.setText(errorMessage);
+            }
+        };
+        SongListRequestListener thisWeekListener = new SongListRequestListener() {
+            @Override
+            public void didFetch(SongResponse response) {
+                rvThisWeekHits.setAdapter(new SongAdapter(HitsActivity.this, response.getResults(),
+                        (position, v, id) -> {
+                            Intent intent = new Intent(HitsActivity.this, SongActivity.class);
+                            intent.putExtra("id", id);
+                            startActivity(intent);
+                        }));
+            }
+
+            @Override
+            public void didError(String errorMessage) {
+                tvError.setText(errorMessage);
+            }
+        };
+        manager.getTopHitsToday(todayListener);
+        manager.getTopHitsThisWeek(thisWeekListener);
     }
 }
